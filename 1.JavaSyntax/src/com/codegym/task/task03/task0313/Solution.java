7bb163ffcb63412061f83e23a85e40c997c4033c
IGNovikov <IGNovikov@esphere.ru>
2019-01-16 19:38:07 +0300

    add 0313

---
+++ 1.JavaSyntax/src/com/codegym/task/task03/task0313/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task03.task0313;
+
+/* 
+Sam I Am
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println("SamIAm");
+        System.out.println("SamAmI");
+        System.out.println("AmISam");
+        System.out.println("AmSamI");
+        System.out.println("IAmSam");
+        System.out.println("ISamAm");
+    }
+}
