4f79df7d1b3996842666fbec0591a36261f86ae9
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 12:21:43 +0300

    add 0325

---
+++ 1.JavaSyntax/src/com/codegym/task/task03/task0325/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task03.task0325;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Financial expectations
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.nextLine();
+        System.out.println("I will earn $" + n + " per hour");
+    }
+}
