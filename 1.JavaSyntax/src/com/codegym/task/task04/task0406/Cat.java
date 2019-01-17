e8d3e9521f2efb825d0529f504811d3cb00dc3b7
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 14:38:20 +0300

    add 0406

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0406/Cat.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task04.task0406;
+
+/* 
+Name register
+
+*/
+
+public class Cat {
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+        this.fullName = fullName;
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
