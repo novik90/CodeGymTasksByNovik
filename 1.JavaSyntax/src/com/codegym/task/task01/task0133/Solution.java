35ef16c37e9c69d7c73f799374f2383e44bd4e2b
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 12:05:04 +0300

    add 0133

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0133/Solution.java
@@ -0,0 +1,13 @@
+package com.codegym.task.task01.task0133;
+
+/* 
+Don't think about seconds…
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int secondsAfter15 = 0;
+        System.out.println(secondsAfter15 + 30 * 60);
+    }
+}
