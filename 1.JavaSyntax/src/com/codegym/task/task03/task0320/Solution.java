1336d3a57642957375c31eb955f5aef90d0e1225
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 11:49:46 +0300

    add 0320

---
+++ 1.JavaSyntax/src/com/codegym/task/task03/task0320/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task03.task0320;
+
+
+/* 
+The humble programmer
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(name + " makes $120,000 a year. Ha-ha-ha!");
+    }
+}
