78f0861ab74dbb05a101b5a5087e41961b62973f
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 11:56:34 +0300

    add 0131

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0131/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task01.task0131;
+
+/* 
+More conversions
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getFeetFromInches(28));
+    }
+
+    public static int getFeetFromInches(int inches) {
+        //write your code here
+        return inches / 12;
+    }
+}
