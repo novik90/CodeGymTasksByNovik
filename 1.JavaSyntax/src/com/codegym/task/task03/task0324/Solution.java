aae1277376c84e35d1f9c574d8dcb6c3a7cbaae2
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 12:18:33 +0300

    add 0324

---
+++ 1.JavaSyntax/src/com/codegym/task/task03/task0324/Solution.java
@@ -0,0 +1,15 @@
+package com.codegym.task.task03.task0324;
+
+/* 
+Mercantile intentions
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I want a big salary, and that's why I'm studying Java");
+        }
+    }
+}
