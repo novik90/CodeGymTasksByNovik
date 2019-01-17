af0abb3a6e8972ca7877382fa79cfd1b75d8a416
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 11:52:58 +0300

    add 0322

---
+++ 1.JavaSyntax/src/com/codegym/task/task03/task0322/Solution.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task03.task0322;
+
+
+/* 
+Deep and pure love
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.nextLine();
+        String name2 = scanner.nextLine();
+        String name3 = scanner.nextLine();
+
+        System.out.println(name1 + " + " + name2 + " + " + name3 + " = Pure love. Ooo la-la!");
+    }
+}
