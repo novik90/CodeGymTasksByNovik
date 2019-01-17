915aefc5abfa505b2b37d54be01401fb8776b0c3
IGNovikov <IGNovikov@esphere.ru>
2019-01-17 15:19:19 +0300

    add 0412

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0412/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task04.task0412;
+
+/* 
+Positive and negative numbers
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        Scanner scanner = new Scanner(System.in);
+        String inputText = scanner.nextLine();
+        int num = Integer.parseInt(inputText);
+        int result = 0;
+        if (num > 0) {
+            result = num * 2;
+        } else if (num < 0) {
+            result = num;
+        } else {
+            result = 0;
+        }
+        System.out.println(result);
+    }
+
+}
