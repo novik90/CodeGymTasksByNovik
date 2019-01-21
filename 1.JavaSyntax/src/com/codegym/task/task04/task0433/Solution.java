f6fbd98b652a84fb5abb3ddd598e8109c5f8b301
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 11:57:44 +0300

    Add 0433

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0433/Solution.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task04.task0433;
+
+
+/* 
+Seeing dollars in your future
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        String s = "$$$$$$$$$$";
+        int count = 0;
+
+        while (count < 10){
+            System.out.println(s);
+            count++;
+        }
+    }
+}
