fca3cdf6a10f35b312e96385eaa5e5060bf5e25e
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 19:19:59 +0300

    Add 0530

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0530/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task05.task0530;
+
+import java.io.*;
+
+/* 
+Boss, something weird is happening
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
