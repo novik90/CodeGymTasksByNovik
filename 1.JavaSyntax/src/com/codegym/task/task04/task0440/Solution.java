d61ab741f7ff8b7cb73037203242daf6120aa58b
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 12:55:22 +0300

    Add 0440

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0440/Solution.java
@@ -0,0 +1,15 @@
+package com.codegym.task.task04.task0440;
+
+/* 
+Decent pay
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I will never work for peanuts. Amigo");
+        }
+    }
+}
