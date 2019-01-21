e23c8b0e660acfca696b0cb220ae78b388b855f5
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 16:20:40 +0300

    Add 0504

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0504/Solution.java
@@ -0,0 +1,30 @@
+package com.codegym.task.task05.task0504;
+
+
+/* 
+The Three "Muscateers"
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        Cat cat1 = new Cat("Cat 1", 4, 4, 5);
+        Cat cat2 = new Cat("Cat 2", 5, 5, 7);
+        Cat cat3 = new Cat("Cat 3", 8, 5, 2);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
