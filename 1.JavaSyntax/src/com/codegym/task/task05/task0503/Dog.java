2a0a759ee45f130e1b22b6157cfa45a08763b0ba
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 16:17:17 +0300

    Add 0503

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0503/Dog.java
@@ -0,0 +1,33 @@
+package com.codegym.task.task05.task0503;
+
+
+/* 
+Getters and setters for the Dog class
+
+*/
+
+public class Dog {
+    //write your code here
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
