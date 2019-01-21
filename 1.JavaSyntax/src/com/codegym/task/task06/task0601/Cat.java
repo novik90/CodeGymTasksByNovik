eca0b51005f2cd03cea0d45a66b4a271f23acf54
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 21:20:22 +0300

    Add 0601

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0601/Cat.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task06.task0601;
+
+/* 
+Cat's finalize method
+
+*/
+
+public class Cat {
+
+    protected void finalize() throws Throwable{
+        System.out.println("Hello!");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
