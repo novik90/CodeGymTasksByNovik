6e44332e0a6c3e95483667a9ecd6f56371e5855c
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 21:01:45 +0300

    Add 0532

--- 1.JavaSyntax/src/com/codegym/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0532/Solution.java
@@ -10,7 +10,24 @@ Task about algorithms
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = 0;
-        System.out.println(maximum);
+        int maximum;
+        int count = 0;
+        int i = 0;
+        int j = Integer.parseInt(reader.readLine());
+
+        while (i < j){
+            if (i == 0) {
+                maximum = Integer.parseInt(reader.readLine());
+                count = maximum;
+            } else {
+                maximum = Integer.parseInt(reader.readLine());
+                if (maximum > count) {
+                    count = maximum;
+                }
+            }
+            i++;
+        }
+
+        System.out.println(count);
     }
 }
