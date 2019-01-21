0855043d9e3d2edc01400f404d76cd7f3a7fe0e6
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 11:49:26 +0300

    Add 0431

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0431/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task04.task0431;
+
+/* 
+From 10 to 1
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        int exit = 10;
+        while (exit > 0) {
+            System.out.println(exit);
+            exit--;
+        }
+    }
+}
