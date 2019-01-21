2b8a31bac39ddc03c7b00cf30e36f941ec60754a
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 18:14:33 +0300

    Add 0519

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0519/Circle.java
@@ -0,0 +1,37 @@
+package com.codegym.task.task05.task0519;
+
+/* 
+Walking in circles
+
+*/
+
+
+public class Circle {
+    //write your code here
+    int centerX, centerY, radius, width, color;
+
+    public Circle(int centerX, int centerY, int radius, int width, int color) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+        this.color = color;
+    }
+
+    public Circle(int centerX, int centerY, int radius, int width) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+    }
+
+    public Circle(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
