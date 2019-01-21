edb72aeb419214d0fd436106e89047c0dbbf958f
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 19:08:38 +0300

    Add 0528

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0528/Solution.java
@@ -0,0 +1,13 @@
+package com.codegym.task.task05.task0528;
+
+/* 
+Display today's date
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println("01 21 2019");
+    }
+}
