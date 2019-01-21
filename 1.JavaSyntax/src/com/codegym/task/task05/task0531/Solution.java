7b50158f1061b4ebd2358151691fc4d5d7f21a8a
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 19:27:12 +0300

    Add 0531

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0531/Solution.java
@@ -0,0 +1,41 @@
+package com.codegym.task.task05.task0531;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Improving functionality
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b, c, d, e);
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    public static int min(int a, int b, int c, int d, int e) {
+        int result1;
+        int result2;
+        int result3;
+
+        result1 = min(a, b);
+        result2 = min(c, d);
+        result3 = min(result1, result2);
+        return min(result3, e);
+    }
+}
