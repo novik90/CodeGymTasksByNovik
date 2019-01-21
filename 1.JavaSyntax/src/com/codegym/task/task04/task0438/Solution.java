60a4bf3a8d8e45e181b8d7dba5996290b51e03c6
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 12:41:47 +0300

    Add 0438

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0438/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task04.task0438;
+
+/* 
+Drawing lines
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        int n = 0;
+        for (int i = 0; i < 10; i++) {
+            while (n < 9) {
+                System.out.print(8);
+                n++;
+            }
+            System.out.println(8);
+        }
+    }
+}
