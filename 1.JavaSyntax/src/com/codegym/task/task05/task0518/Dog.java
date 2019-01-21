05eb86d776add92537ba5cd79d28fb270f39093a
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 18:12:57 +0300

    Add 0518

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0518/Dog.java
@@ -0,0 +1,33 @@
+package com.codegym.task.task05.task0518;
+
+/* 
+Dog registration
+
+*/
+
+
+public class Dog {
+    //write your code here
+    String name;
+    int height;
+    String color;
+
+    public Dog(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public Dog(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
