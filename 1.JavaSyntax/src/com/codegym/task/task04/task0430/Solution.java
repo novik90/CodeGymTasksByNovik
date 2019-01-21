f61be63f2766699ea5683c1d9bb07913b43df568
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 11:47:13 +0300

    Add 0430

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0430/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task04.task0430;
+
+/* 
+1 to 10
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        int exit = 1;
+        while (exit < 11) {
+            System.out.println(exit);
+            exit++;
+        }
+    }
+}
