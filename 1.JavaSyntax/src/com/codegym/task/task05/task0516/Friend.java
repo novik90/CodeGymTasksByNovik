f1b645a47fad3a24ef6218cab934db6f8e1258a3
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 18:05:59 +0300

    Add 0516

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0516/Friend.java
@@ -0,0 +1,32 @@
+package com.codegym.task.task05.task0516;
+
+/* 
+You can't buy friends
+
+*/
+
+public class Friend {
+    //write your code here
+    String name;
+    int age;
+    char sex;
+
+    public Friend(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Friend(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
