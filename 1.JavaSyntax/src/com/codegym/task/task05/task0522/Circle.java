b6c45bd3701c8e629fb682c3115aa93ec84e4825
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 18:24:23 +0300

    Add 0522

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0522/Circle.java
@@ -0,0 +1,36 @@
+package com.codegym.task.task05.task0522;
+
+/* 
+Max constructors
+
+*/
+
+public class Circle {
+    public double x;
+    public double y;
+    public double radius;
+
+    //write your code here
+
+    public Circle() {
+    }
+
+    public Circle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public Circle(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Circle(double x) {
+        this.x = x;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
