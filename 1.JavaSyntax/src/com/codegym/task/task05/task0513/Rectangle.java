882092ebc56e87c0e8880cb11657b22b1621d538
IGNovikov <IGNovikov@esphere.ru>
2019-01-21 17:50:27 +0300

    Add 0513

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0513/Rectangle.java
@@ -0,0 +1,40 @@
+package com.codegym.task.task05.task0513;
+
+/* 
+Let's put together a rectangle
+
+*/
+
+public class Rectangle {
+    //write your code here
+    int top;
+    int left;
+    int width;
+    int height;
+
+    public void initialize(int top) {
+        this.top = top;
+    }
+
+    public void initialize(int top, int left) {
+        this.top = top;
+        this.left = left;
+    }
+
+    public void initialize(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+    }
+
+    public void initialize(int top, int left, int width, int height) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
