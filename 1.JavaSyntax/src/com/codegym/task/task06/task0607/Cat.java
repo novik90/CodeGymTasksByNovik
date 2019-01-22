d476004474e7673e6ef75bfea99fc4a1d8e0f874
IGNovikov <IGNovikov@esphere.ru>
2019-01-22 10:38:18 +0300

    Add 0607

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0607/Cat.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task06.task0607;
+
+/* 
+Class counter
+
+*/
+
+public class Cat {
+
+    static int catCount;
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
