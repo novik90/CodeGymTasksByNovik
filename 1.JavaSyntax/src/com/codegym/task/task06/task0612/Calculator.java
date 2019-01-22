f81592587b958740b2929a3adee7cfbf2e563079
IGNovikov <IGNovikov@esphere.ru>
2019-01-22 12:45:33 +0300

    add 0612

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0612/Calculator.java
@@ -0,0 +1,37 @@
+package com.codegym.task.task06.task0612;
+
+/* 
+Calculator
+
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        //write your code here
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        //write your code here
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        //write your code here
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        //write your code here
+        return a / (double) b;
+    }
+
+    public static double percent(int a, int b) {
+        //write your code here
+        return ((double) a / 100) * b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
