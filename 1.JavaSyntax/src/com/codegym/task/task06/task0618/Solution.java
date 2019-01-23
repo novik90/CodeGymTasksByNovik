9afe86055158cfafe02babad2b3e07b2725ffc52
novik90 <ivan.novikov.1990@list.ru>
2019-01-23 09:53:39 +0300

    add 0618

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0618/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task06.task0618;
+
+/* 
+KissMyShinyMetalRearActuator
+
+*/
+
+public class Solution {
+    public static class KissMyShinyMetalRearActuator {
+        static String shinyMetalRearActuator = "Kiss My Shiny Metal Rear Actuator";
+    }
+
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println(KissMyShinyMetalRearActuator.shinyMetalRearActuator);
+    }
+}
