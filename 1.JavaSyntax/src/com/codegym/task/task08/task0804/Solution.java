88a46d26b4178086883e049fe0332cb916d06a6f
novik90 <ivan.novikov.1990@list.ru>
2019-01-29 16:12:55 +0300

    add 0804 "Display a list of keys"

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0804/Solution.java
@@ -0,0 +1,33 @@
+package com.codegym.task.task08.task0804;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Display a list of keys
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("Sim", "Sim");
+        map.put("Tom", "Tom");
+        map.put("Arbus", "Arbus");
+        map.put("Baby", "Baby");
+        map.put("Cat", "Cat");
+        map.put("Dog", "Dog");
+        map.put("Eat", "Eat");
+        map.put("Food", "Food");
+        map.put("Gevey", "Gevey");
+        map.put("Hugs", "Hugs");
+
+        printKeys(map);
+    }
+
+    public static void printKeys(Map<String, String> map) {
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            System.out.println(pair.getKey());
+        }
+    }
+}
