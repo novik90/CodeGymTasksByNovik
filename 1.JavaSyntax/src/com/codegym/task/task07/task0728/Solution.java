ddca6fc0b1100309e4cff2df4e68a707dda9a9ce
novik90 <ivan.novikov.1990@list.ru>
2019-01-29 10:43:40 +0300

    add 0728 "In decreasing order"

---
+++ 1.JavaSyntax/src/com/codegym/task/task07/task0728/Solution.java
@@ -0,0 +1,38 @@
+package com.codegym.task.task07.task0728;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.ForkJoinPool;
+
+/* 
+In decreasing order
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
