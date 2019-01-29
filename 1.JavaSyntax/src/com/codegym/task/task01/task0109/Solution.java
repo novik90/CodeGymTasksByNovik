beb622d09bc41e34b6a44ab9f458d6a139c29561
novik90 <ivan.novikov.1990@list.ru>
2019-01-29 16:47:51 +0300

    add 0109 "We solemnly publish and declare these variables..."

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.codegym.task.task01.task0109;
+
+/* 
+We solemnly publish and declare these variables...
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+    }
+}
