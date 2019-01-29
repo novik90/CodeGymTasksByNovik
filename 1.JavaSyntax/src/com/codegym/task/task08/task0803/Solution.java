967722fd90f677566587d2a40bc5bec4fc090f32
novik90 <ivan.novikov.1990@list.ru>
2019-01-29 16:03:40 +0300

    add 0803 "HashMap of cats"

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0803/Solution.java
@@ -0,0 +1,45 @@
+package com.codegym.task.task08.task0803;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/* 
+HashMap of cats
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String[] cats = new String[]{"Tiger", "Missy", "Smokey", "Marmalade", "Oscar", "Snowball", "Boss", "Smudge", "Max", "Simba"};
+
+        HashMap<String, Cat> map = addCatsToMap(cats);
+
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+        }
+    }
+
+
+    public static HashMap<String, Cat> addCatsToMap(String[] cats) {
+        HashMap<String, Cat> result = new HashMap<>();
+        for (String s : cats) {
+            result.put(new Cat(s).name, new Cat(s));
+        }
+        return result;
+    }
+
+
+    public static class Cat {
+        String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name != null ? name.toUpperCase() : null;
+        }
+    }
+}
