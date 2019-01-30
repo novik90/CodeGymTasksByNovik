1051f8f5b048de8f2aa8956e572b9995380ed991
novik90 <ivan.novikov.1990@list.ru>
2019-01-30 09:48:09 +0300

    update 0411 "Seasons on Terra"

--- 1.JavaSyntax/src/com/codegym/task/task04/task0411/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0411/Solution.java
@@ -7,22 +7,37 @@ Seasons on Terra
 
 public class Solution {
     public static void main(String[] args) {
-        checkSeason(12);
+        checkSeason(1);
+        checkSeason(2);
+        checkSeason(3);
         checkSeason(4);
+        checkSeason(5);
+        checkSeason(6);
         checkSeason(7);
+        checkSeason(8);
+        checkSeason(9);
         checkSeason(10);
+        checkSeason(11);
+        checkSeason(12);
+        checkSeason(13);
+        checkSeason(0);
     }
 
     public static void checkSeason(int month) {
-        //write your code here
-        if (month == 12 || month == 1 || month == 2) {
-            System.out.println("winter");
-        } else if (month == 3 || month == 4 || month == 5) {
-            System.out.println("spring");
-        } else if (month == 6 || month == 7 || month == 8) {
-            System.out.println("summer");
+        if (month > 0 && month < 13) {
+            if (month < 3) {
+                System.out.println("winter");
+            } else if (month < 6) {
+                System.out.println("spring");
+            } else if (month < 9) {
+                System.out.println("summer");
+            } else if (month < 12) {
+                System.out.println("autumn");
+            } else {
+                System.out.println("winter");
+            }
         } else {
-            System.out.println("autumn");
+            System.out.println("not a month");
         }
     }
 }
