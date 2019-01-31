06e9bf1e909042d295523e5a690d15e6df1596f5
Novik <ivan.novikov.1990@list.ru>
2019-01-31 21:24:21 +0300

    0416 refactor

--- 1.JavaSyntax/src/com/codegym/task/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0416/Solution.java
@@ -5,26 +5,13 @@ Crossing the road blindly
 
 */
 
-import java.io.*;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //write your code here
         Scanner scanner = new Scanner(System.in);
-        String numberInput = scanner.nextLine();
-        double number = Double.parseDouble(numberInput);
-        double result = number;
-
-        if (number > 4)
-            result = number % 5;
-
-        if (result < 3) {
-            System.out.println("green");
-        } else if (result < 4) {
-            System.out.println("yellow");
-        } else {
-            System.out.println("red");
-        }
+        double number = scanner.nextDouble();
+        String[] s = {"green", "green", "green", "yellow", "red"};
+        System.out.println(s[(int)number % 5]);
     }
 }
