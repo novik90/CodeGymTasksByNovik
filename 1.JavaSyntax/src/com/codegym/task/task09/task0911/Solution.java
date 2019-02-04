d980f6173bae9a090728853b9f66dfcaa7fc1a56
Novik <ivan.novikov.1990@list.ru>
2019-02-04 22:01:27 +0300

    add 0911 "Exception when working with Map collections"

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0911/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task09.task0911;
+
+import java.util.HashMap;
+
+/* 
+Exception when working with Map collections
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try {
+            HashMap<String, String> map = new HashMap<String, String>(null);
+            map.put(null, null);
+            map.remove(null);
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
+    }
+}
