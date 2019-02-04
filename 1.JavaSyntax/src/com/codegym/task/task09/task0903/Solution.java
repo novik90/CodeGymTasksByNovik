1c79662e445cef8a0914933e1c78fa489c604528
novik90 <ivan.novikov.1990@list.ru>
2019-02-04 15:14:51 +0300

    added 0903 "Who called me?"

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0903/Solution.java
@@ -0,0 +1,36 @@
+package com.codegym.task.task09.task0903;
+
+/* 
+Who called me?
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static int method1() {
+        method2();
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method2() {
+        method3();
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method3() {
+        method4();
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method4() {
+        method5();
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method5() {
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+}
