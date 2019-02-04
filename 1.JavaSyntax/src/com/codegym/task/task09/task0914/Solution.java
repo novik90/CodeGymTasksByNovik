448fc4eb19f8f45ccc9a96512ee46195778b881c
Novik <ivan.novikov.1990@list.ru>
2019-02-04 23:45:36 +0300

    add 0914 "Catching a group of exceptions"

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0914/Solution.java
@@ -0,0 +1,40 @@
+package com.codegym.task.task09.task0914;
+
+/* 
+Catching a group of exceptions
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+        try {
+            method1();
+        } catch (Exception1 e) {
+            System.out.println(e);
+        }
+
+        //write your code here
+
+    }
+
+    public static void method1() throws Exception1, Exception2, Exception3 {
+        int i = (int) (Math.random() * 3);
+        if (i == 0)
+            throw new Exception1();
+        if (i == 1)
+            throw new Exception2();
+        if (i == 2)
+            throw new Exception3();
+    }
+}
+
+class Exception1 extends Exception {
+}
+
+class Exception2 extends Exception1 {
+}
+
+class Exception3 extends Exception2 {
+}
