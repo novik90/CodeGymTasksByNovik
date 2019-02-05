9c928b0f72492195e1d6f3d31ca1854b924fd524
novik90 <ivan.novikov.1990@list.ru>
2019-02-05 16:14:41 +0300

    add 0916 "Catching checked exceptions"

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0916/Solution.java
@@ -0,0 +1,38 @@
+package com.codegym.task.task09.task0916;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+
+/* 
+Catching checked exceptions
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        handleExceptions(new Solution());
+
+    }
+
+    public static void handleExceptions(Solution obj) {
+        try {
+            obj.method1();
+            obj.method2();
+            obj.method3();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void method1() throws IOException {
+        throw new IOException();
+    }
+
+    public void method2() throws NoSuchFieldException {
+        throw new NoSuchFieldException();
+    }
+
+    public void method3() throws RemoteException {
+        throw new RemoteException();
+    }
+}
