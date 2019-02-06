695debc2b73d34d096993f1fd2a8bff100376d15
novik90 <ivan.novikov.1990@list.ru>
2019-02-06 10:44:29 +0300

    add 0918 "We're all friends here, even the exceptions"

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0918/Solution.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task09.task0918;
+
+/* 
+We're all friends here, even the exceptions
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+    
+    static class MyException extends Exception{
+    }
+
+    static class MyException2 extends Exception {
+    }
+
+    static class MyException3 extends RuntimeException {
+    }
+
+    static class MyException4 extends RuntimeException {
+    }
+}
+
