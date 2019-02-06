739487e19dde398accadcb36017e5662b19aa797
novik90 <ivan.novikov.1990@list.ru>
2019-02-06 15:53:20 +0300

    add 0921 "Method in a try-catch"

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0921/Solution.java
@@ -0,0 +1,30 @@
+package com.codegym.task.task09.task0921;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* 
+Method in a try-catch
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        readData();
+    }
+
+    public static void readData() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> num = new ArrayList<>();
+        try {
+            while (true) {
+                num.add(scanner.nextInt());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (Integer i : num) {
+            System.out.println(i);
+        }
+    }
+}
