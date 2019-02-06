8e9f63485ea9545e53b3be117f242fe613065136
novik90 <ivan.novikov.1990@list.ru>
2019-02-06 15:29:23 +0300

    add 0920 "Countdown"

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0920/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task09.task0920;
+
+/* 
+Countdown
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
