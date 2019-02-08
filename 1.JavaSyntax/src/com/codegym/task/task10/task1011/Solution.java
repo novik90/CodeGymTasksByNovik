6b8fb1d9580d5ccb3eed740c6e04619e961d207c
novik90 <ivan.novikov.1990@list.ru>
2019-02-08 14:27:21 +0300

    add 1011 "Big salary"

---
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1011/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task10.task1011;
+
+/* 
+Big salary
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "I do not want to learn Java. I want a big salary";
+        for (int i = 0; i < 43; i++) {
+            System.out.println(s.substring(i));
+        }
+    }
+
+}
+
