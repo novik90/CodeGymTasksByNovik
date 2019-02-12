19eddcbf426d80b531636b1a1e0f6964b90fce16
novik90 <ivan.novikov.1990@list.ru>
2019-02-12 17:29:54 +0300

    add 1020 "Task about algorithms"

---
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1020/Solution.java
@@ -0,0 +1,37 @@
+package com.codegym.task.task10.task1020;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/* 
+Task about algorithms
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[30];
+        for (int i = 0; i < 30; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[9]);
+        System.out.println(array[10]);
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i +1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
+}
