eef0cb378b7c581eb0ce62e817d199b3518d82c0
novik90 <ivan.novikov.1990@list.ru>
2019-02-12 15:03:58 +0300

    add 1016 "Identical words in a list"

---
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1016/Solution.java
@@ -0,0 +1,43 @@
+package com.codegym.task.task10.task1016;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/* 
+Identical words in a list
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> words = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            words.add(reader.readLine());
+        }
+
+        Map<String, Integer> map = countWords(words);
+
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
+
+    public static Map<String, Integer> countWords(ArrayList<String> list) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+        for (String key : list) {
+            if (!result.containsKey(key)) {
+                result.put(key, 0);
+            }
+            result.put(key, result.get(key) + 1);
+        }
+        return result;
+    }
+
+}
+
