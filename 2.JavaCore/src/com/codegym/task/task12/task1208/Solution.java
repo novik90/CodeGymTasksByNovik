086528006d2bf44f9818631c507c23f080d30d20
novik90 <ivan.novikov.1990@list.ru>
2019-02-14 16:19:38 +0300

    add 1208 "Freedom of the press"

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.codegym.task.task12.task1208;
+
+/* 
+Freedom of the press
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(byte b) {
+
+    }
+
+    static void print(int i) {
+
+    }
+
+    static void print(String s) {
+
+    }
+
+    static void print(char c) {
+
+    }
+
+    static void print(boolean b) {
+
+    }
+}
