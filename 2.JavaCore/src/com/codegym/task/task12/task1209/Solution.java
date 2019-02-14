80adfea7b9e2da2ba17426e393acc2d44da16043
novik90 <ivan.novikov.1990@list.ru>
2019-02-14 16:24:05 +0300

    add 1209 "Three methods and a minimum"

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1209/Solution.java
@@ -0,0 +1,33 @@
+package com.codegym.task.task12.task1209;
+
+/* 
+Three methods and a minimum
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static int min(int a, int b) {
+        if (a > b) {
+            return b;
+        }
+        return a;
+    }
+
+    static long min(long a, long b) {
+        if (a > b) {
+            return b;
+        }
+        return a;
+    }
+
+    static double min(double a, double b) {
+        if (a > b) {
+            return b;
+        }
+        return a;
+    }
+}
