eca28c77d7a7c19477a930c7853d731c0a6ef828
novik90 <ivan.novikov.1990@list.ru>
2019-02-14 22:26:40 +0300

    add 1217 "Fly, run and swim"

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task12.task1217;
+
+/* 
+Fly, run and swim
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly();
+    }
+    public interface CanRun {
+        void run();
+    }
+    public interface CanSwim {
+        void swim();
+    }
+
+}
