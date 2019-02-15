4e07a11e15b4376b37513f717d8e7ed575ebf4b4
novik90 <ivan.novikov.1990@list.ru>
2019-02-15 12:45:34 +0300

    add 1228 "Interfaces for the Human class"

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1228/Solution.java
@@ -0,0 +1,38 @@
+package com.codegym.task.task12.task1228;
+
+/* 
+Interfaces for the Human class
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human);
+    }
+
+    public static interface Employee {
+        public void workLazily();
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static interface Secretary {
+        public void workLazily();
+    }
+
+    public static interface Miner {
+        public void workVeryHard();
+    }
+
+    public static class Human implements Businessman, Employee, Secretary {
+
+        public void workHard() {
+        }
+
+        public void workLazily() {
+        }
+    }
+}
