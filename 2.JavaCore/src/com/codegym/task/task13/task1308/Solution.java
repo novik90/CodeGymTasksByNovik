5b9fc8c4834e18d404a2741e74121ed7e24c5085
Novik <ivan.novikov.1990@list.ru>
2019-02-16 21:04:31 +0300

    add 1308 "Hey, are you alive?"

---
+++ 2.JavaCore/src/com/codegym/task/task13/task1308/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task13.task1308;
+
+/* 
+Hey, are you alive?
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    interface Person {
+        boolean isAlive();
+    }
+
+    interface Presentable extends Person {
+
+    }
+}
