c977f0458e2ff364bf24c99e45399b98ce1bcb4f
Novik <ivan.novikov.1990@list.ru>
2019-02-17 13:06:32 +0300

    add 1318 "Reading a file"

---
+++ 2.JavaCore/src/com/codegym/task/task13/task1318/Solution.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task13.task1318;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Reading a file
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            System.out.print((char) data);
+        }
+        fileInputStream.close();
+        bufferedReader.close();
+    }
+}
