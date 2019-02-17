7d99d1851ae4eb376d2f493e6ab2debef5520646
Novik <ivan.novikov.1990@list.ru>
2019-02-17 12:44:58 +0300

    add 1316 "Incorrect lines"

---
+++ 2.JavaCore/src/com/codegym/task/task13/task1316/Solution.java
@@ -0,0 +1,29 @@
+package com.codegym.task.task13.task1316;
+
+/* 
+Incorrect lines
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println(SimpleObject.NAME);
+        System.out.println(Button.NAME);
+    }
+
+    interface SimpleObject {
+        String NAME = "SimpleObject";
+
+        void onPress();
+    }
+
+    interface Button extends SimpleObject {
+
+        final String NAME = "Submit";
+
+        void onPress();
+
+        String onPress(Object o);
+
+    }
+}
