eaa9aa6e1c798159e2145c92f79c3d5168ca6513
novik90 <ivan.novikov.1990@list.ru>
2019-02-21 10:24:30 +0300

    add 1410 "Wine tasting"

---
+++ 2.JavaCore/src/com/codegym/task/task14/task1410/Drink.java
@@ -0,0 +1,12 @@
+package com.codegym.task.task14.task1410;
+
+public abstract class Drink {
+    public void taste() {
+        System.out.println("Delicious");
+    }
+
+    public String getCelebrationName() {
+        return "Delicious";
+    }
+
+}
---
+++ 2.JavaCore/src/com/codegym/task/task14/task1410/Wine.java
@@ -0,0 +1,8 @@
+package com.codegym.task.task14.task1410;
+
+public class Wine extends Drink {
+    @Override
+    public String getCelebrationName() {
+        return "Birthday";
+    }
+}
