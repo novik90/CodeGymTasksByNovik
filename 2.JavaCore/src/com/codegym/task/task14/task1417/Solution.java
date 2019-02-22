d3692a1f8a90a5dd7ef0fe4b8d61d98a1aeaa0ae
novik90 <ivan.novikov.1990@list.ru>
2019-02-22 11:56:55 +0300

    add 1417 "Currencies"

---
+++ 2.JavaCore/src/com/codegym/task/task14/task1417/Solution.java
@@ -0,0 +1,35 @@
+package com.codegym.task.task14.task1417;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Currencies
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person paul = new Person("Paul");
+        for (Money money : paul.getAllMoney()) {
+            System.out.println(paul.name + " has a stash of " + money.getAmount() + " " + money.getCurrencyName());
+        }
+    }
+
+    static class Person {
+        public String name;
+        private List<Money> allMoney;
+
+        Person(String name) {
+            this.name = name;
+            this.allMoney = new ArrayList<Money>();
+            this.allMoney.add(new Ruble(5));
+            this.allMoney.add(new Euro(1));
+            this.allMoney.add(new USD(2));
+        }
+
+        public List<Money> getAllMoney() {
+            return allMoney;
+        }
+    }
+}
