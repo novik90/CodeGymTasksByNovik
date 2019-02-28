288e26d062c30a7831989266da717798f311dcd6
novik90 <ivan.novikov.1990@list.ru>
2019-02-28 15:58:52 +0300

    add 1508 "OOP: Method overloading - Eliminating the superfluous"

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1508/Solution.java
@@ -0,0 +1,32 @@
+package com.codegym.task.task15.task1508;
+
+/* 
+OOP: Method overloading - Eliminating the superfluous
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+//    public static void print(byte b) {
+//        System.out.println("I will be a Java programmer!");
+//    }
+
+//    public static void print(double d) {
+//        System.out.println("I will be a Java programmer!");
+//    }
+
+    public static void print(long l) {
+        System.out.println("I will be a Java programmer!");
+    }
+
+//    public static void print(float f) {
+//        System.out.println("I will be a Java programmer!");
+//    }
+
+//    public static void print(char c) {
+//        System.out.println("I will be a Java programmer!");
+//    }
+}
