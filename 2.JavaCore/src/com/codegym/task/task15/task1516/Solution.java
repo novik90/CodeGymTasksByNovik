218af3ba25f7e1719a54d8307e02e9584f6e376c
novik90 <ivan.novikov.1990@list.ru>
2019-03-03 15:08:46 +0300

    add 1516 "Default values"

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1516/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task15.task1516;
+
+/* 
+Default values
+
+*/
+
+public class Solution {
+
+    public int intVar;
+    public double doubleVar;
+    public Double DoubleVar;
+    public boolean booleanVar;
+    public Object ObjectVar;
+    public Exception ExceptionVar;
+    public String StringVar;
+
+    public static void main(String[] args) throws NoSuchFieldException {
+        Solution solution = new Solution();
+        System.out.println(solution.intVar);
+        System.out.println(solution.doubleVar);
+        System.out.println(solution.DoubleVar);
+        System.out.println(solution.booleanVar);
+        System.out.println(solution.ObjectVar);
+        System.out.println(solution.ExceptionVar);
+        System.out.println(solution.StringVar);
+    }
+}
