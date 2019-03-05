0c6c460c8e3b647e217ab2d689389630c9ffb023
novik90 <ivan.novikov.1990@list.ru>
2019-03-06 00:02:30 +0300

    add 1528 "OOP: The Euro is money too"

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1528/Solution.java
@@ -0,0 +1,33 @@
+package com.codegym.task.task15.task1528;
+
+/* 
+OOP: The Euro is money too
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Euro().getAmount());
+    }
+
+    public static abstract class Money {
+        abstract Money getMoney();
+
+        public Object getAmount() {
+            return getMoney().getAmount();
+        }
+    }
+
+    // Add your code below
+    public static class Euro extends Money {
+        private double amount = 123d;
+
+        public Euro getMoney() {
+            return this;
+        }
+
+        public Object getAmount() {
+            return getMoney().amount;
+        }
+    }
+}
