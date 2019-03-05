809ce17029a8845d81c6ab762a42fba39ea82fb0
novik90 <ivan.novikov.1990@list.ru>
2019-03-05 21:42:31 +0300

    add 1523 "Overloading constructors"

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task15.task1523;
+
+/* 
+Overloading constructors
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    private Solution(String s) {
+
+    }
+
+    protected Solution(int i) {
+
+    }
+
+    public Solution (double d) {
+
+    }
+
+    Solution(boolean b) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/codegym/task/task15/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    protected SubSolution(int i) {
+        super(i);
+    }
+
+    public SubSolution(double d) {
+        super(d);
+    }
+
+    SubSolution(boolean b) {
+        super(b);
+    }
+
+}
