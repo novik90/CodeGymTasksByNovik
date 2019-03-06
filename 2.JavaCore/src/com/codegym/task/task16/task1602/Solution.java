2ee866c9331a297d1dd886210710659a39b7310a
novik90 <ivan.novikov.1990@list.ru>
2019-03-06 14:09:48 +0300

    add 1602 "My second thread"

---
+++ 2.JavaCore/src/com/codegym/task/task16/task1602/Solution.java
@@ -0,0 +1,25 @@
+package com.codegym.task.task16.task1602;
+
+/* 
+My second thread
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        TestThread thread = new TestThread();
+        thread.start();
+    }
+
+    public static class TestThread extends Thread {
+
+        static {
+            System.out.println("This is the static block inside TestThread");
+        }
+
+        @Override
+        public void run() {
+            System.out.println("This is the run method");
+        }
+    }
+}
